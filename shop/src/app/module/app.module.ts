2f01cdb198316e37c7d47d7d4ef3cdc5cb4994db
Vrezh <vrezhunanyan1997@mail.ru>
2018-05-26 17:19:33 +0400

    Added login screen.

--- shop/src/app/module/app.module.ts
+++ shop/src/app/module/app.module.ts
@@ -1,16 +1,28 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpModule, JsonpModule } from '@angular/http';
 
+// components
 import { AppComponent } from './../component/app/app.component';
 import { NavbarComponent } from './../component/navBar/navBar.component';
+import { LoginComponent } from './../component/login/login.component';
+import { routing } from './../app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
